23a5c5354a8ad73c64474d129d3958ac47c7ab13
caronteone <caronteone@yahoo.it>
2019-02-23 12:23:56 +0100

    asd

---
+++ albums/index.android.js
@@ -0,0 +1,7 @@
+// Import a library to create component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// create component
+
+//render it to device
---
+++ albums/index.ios.js
@@ -0,0 +1,12 @@
+// Import a library to create component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// create component
+const App = () => {
+    return (
+        <Text> Some Text </Text>
+    )
+
+};
+//render it to device
